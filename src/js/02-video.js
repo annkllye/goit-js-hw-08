e95194a54c6313dd98a677906ee3c3f763e591c4
annkllye <annkllye@gmail.com>
2023-03-14 19:41:17 +0300

    2nd-task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
+import playerEl from '@vimeo/player';
 
+const iframeEl = document.querySelector('iframe');
+const player = new playerEl(iframeEl);
+
+
+player.on('timeupdate', throttle(function (t) {
+   localStorage.setItem("videoplayer-current-time",
+      JSON.stringify(t))}, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")).seconds);
