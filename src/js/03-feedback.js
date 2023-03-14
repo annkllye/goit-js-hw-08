1a82f6aca7b6a04c85f668b962f39a10d90fbca8
annkllye <annkllye@gmail.com>
2023-03-14 19:41:26 +0300

    3rd-task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+dataFromLocalStorage();
+
+function onFormData() {
+    const formData = {
+        email: form.elements.email.value,
+        message: form.elements.message.value
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.clear();
+}
+
+function dataFromLocalStorage() {
+  const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedData) {
+    form.elements.email.value = savedData.email;
+    form.elements.message.value = savedData.message;
+  }
+};
+
 
